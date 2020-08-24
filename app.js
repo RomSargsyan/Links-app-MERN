ec375fb8c36be3d95ec5db5c2fa9cb4f34cd1e9b
RomSargsyan <romsargsyan20@gmail.com>
2020-08-24 12:26:47 +0400

    Fixes

--- app.js
+++ app.js
@@ -37,37 +37,3 @@ async function start() {
 }
 
 start()
-
-// const express = require('express')
-// const config = require('config')
-// const path = require('path')
-// const mongoose = require('mongoose')
-
-// const app = express()
-
-// app.use(express.json({ extended: true }))
-
-// app.use('/api/auth', require('./routes/auth'))
-// app.use('/api/link', require('./routes/link'))
-// app.use('/t', require('./routes/redirect'))
-
-
-// const PORT = config.get('port') || 5000
-
-// async function start() {
-//   try {
-//     await mongoose.connect(config.get('mongoUri'), {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//       useCreateIndex: true
-//     })
-//     app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
-//   } catch (e) {
-//     console.log('Server Error', e.message)
-//     process.exit(1)
-//   }
-// }
-
-// start()
-
-
